54631f262be3dce62c3ae78e2ed0599eba36a020
Hadriel Kaplan <hadriel@128technology.com>
2015-08-07 14:31:40 -0400

    Initial commit
    [artf2073]
    
    Change-Id: Ic8df235f3aa9635844c7f65de74f1adf0d6ee092

---
+++ modules/generic/bytes.lua
@@ -0,0 +1,44 @@
+----------------------------------------
+--
+-- Copyright (c) 2015, 128 Technology, Inc.
+--
+-- author: Hadriel Kaplan <hadriel@128technology.com>
+--
+-- This code is licensed under the MIT license.
+--
+-- Version: 1.0
+--
+------------------------------------------
+
+local Settings = require "settings"
+local dprint   = Settings.dprint
+local dprint2  = Settings.dprint2
+local dassert  = Settings.dassert
+local derror   = Settings.derror
+
+local Base = require "generic.base"
+
+
+----------------------------------------
+-- BytesDecoder class, for unknown bytes in LENGTH_DELIMITED fields
+--
+local BytesDecoder = {}
+local BytesDecoder_mt = { __index = BytesDecoder }
+setmetatable( BytesDecoder, { __index = Base } ) -- make it inherit from Base
+
+
+
+function BytesDecoder.new(pfield)
+    local new_class = Base.new("LENGTH_DELIMITED", pfield)
+    setmetatable( new_class, BytesDecoder_mt )
+    return new_class
+end
+
+
+function BytesDecoder:decode(decoder, tag)
+    dprint2("BytesDecoder:decode() called")
+    return decoder:addFieldBytes(self.pfield)
+end
+
+
+return BytesDecoder
---
+++ protobuf.lua
@@ -0,0 +1,67 @@
+-- latest development release of Wireshark supports plugin version information
+if set_plugin_info then
+    local my_info = {
+        version   = "1.0",
+        author    = "Hadriel Kaplan",
+        email     = "hadriel@128technology.com",
+        copyright = "Copyright (c) 2015, 128 Technology, Inc.",
+        license   = "MIT license",
+        details   = "This is a plugin for Wireshark, to dissect Google protobuf messages.",
+        help      = [[
+    HOW TO RUN THIS SCRIPT:
+
+    Wireshark and Tshark support multiple ways of loading Lua scripts: through
+    a dofile() call in init.lua, through the file being in either the global
+    or personal plugins directories, or via the command line. The latter two
+    methods are the best: either copy this script into your "Personal Plugins"
+    directory, or load it from the command line.
+
+    This script also needs to know where to find your '.proto' files. There are
+    two ways of accomplishing that:
+        1. Add it to the "ProtoFileNames" table inside this script, or...
+        2. Add it as a command-line argument(s). This only works if this protobuf.lua
+           script is also loaded through the command line. For example:
+          wireshark -X lua_script:protobuf.lua -X lua_script1:foo.proto -X lua_script1:bar.proto
+    ]]
+    }
+    set_plugin_info(my_info)
+end
+
+
+-- capture command line arguments
+local args = { ... }
+
+
+-- help wireshark find our modules
+package.prepend_path("modules")
+
+
+-- load our settings
+local Settings = require "settings"
+Settings:processCmdLine(args)
+local dprint  = Settings.dprint
+local dprint2 = Settings.dprint2
+
+
+-- load the rest of our modules
+local GenericProto  = require "generic.proto"
+local Compiler = require "compiler"
+
+
+dprint("Protobuf modules loaded")
+
+
+-- initialize the compiler
+Compiler:init()
+
+local proto_files = Settings:getProtoFileNames()
+
+
+----------------------------------------
+-- load the proto file contents
+dprint2(#proto_files,"proto files being compiled")
+
+
+----------------------------------------
+-- compile the proto files
+local decoder_table = Compiler:compile(proto_files)
